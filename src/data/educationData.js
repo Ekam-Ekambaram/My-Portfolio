bc37f234bf85a308713b7f34d514884da0529366
Ekam-Ekambaram <ekmgbekambaram@gmail.com>
2024-08-18 11:12:17 +0530

    first commit

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -3,21 +3,21 @@ export const educationData = [
         id: 1,
         institution: 'Viswa Shanthi High School',
         course: 'Secondary School of Education',
-        startYear: '2016',
-        endYear: '2017'
+       // startYear: '2016',
+       // endYear: '2017'
     },
     {
         id: 2,
         institution: 'Government Polytechnic, GDWL',
         course: 'Diploma of Education',
-        startYear: '2017',
-        endYear: '2020'
+       // startYear: '2017',
+       // endYear: '2020'
     },
     {
         id: 3,
         institution: 'Visvesvaraya College of Engineering and Technology',
         course: 'Bachelor of Technology',
-        startYear: '2020',
-        endYear: '2023'
+       // startYear: '2020',
+       // endYear: '2023'
     },
 ]
